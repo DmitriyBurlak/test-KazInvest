9b8c8b13ef87d135f514e1f81856132910320694
burlak_da <dmshade25@gmail.com>
2026-05-18 16:48:42 +0300

    base settings

---
+++ app/app.vue
@@ -0,0 +1,55 @@
+<script setup>
+import AppLogo from './shared/AppLogo.vue'
+
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'ru'
+  }
+})
+
+const title = 'ChatGPT Web App'
+const description = 'Тестовое задание для KazInvest'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/">
+          <AppLogo class="w-auto h-6 shrink-0" />
+        </NuxtLink>
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+
+        <UButton
+          to="https://github.com/nuxt-ui-templates/starter"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+          color="neutral"
+          variant="ghost"
+        />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+  </UApp>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <UPageHero
+      title="Nuxt Starter Template"
+      description="A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours."
+    />
+  </div>
+</template>
